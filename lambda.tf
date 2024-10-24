f44e436db9c1d411f39badcbf260d92401209fea
Himanshu Jain <54056387+himanshujainhj@users.noreply.github.com>
2024-10-24 16:51:15 +0530

    added 3rd lamda resource

--- lambda.tf
+++ lambda.tf
@@ -23,3 +23,16 @@ resource "aws_lambda_function" "my_lambda-1" {
     security_group_ids = [aws_security_group.lambda_sg.id]
   }
 }
+
+resource "aws_lambda_function" "my_lambda-3" {
+  filename         = "lambda_payload.zip"  
+  function_name    = "my_lambda_function-3"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+}
