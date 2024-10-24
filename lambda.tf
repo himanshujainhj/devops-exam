9fd0a8a784c960aac56915ae504eea01fe92c7fc
Himanshu Jain <54056387+himanshujainhj@users.noreply.github.com>
2024-10-24 16:38:30 +0530

    updated lambda_payload.zip

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,19 @@
 resource "aws_lambda_function" "my_lambda" {
-  filename         = "lambda_payload.zip"  # Your Lambda code ZIP
+  filename         = "lambda_payload.zip" 
   function_name    = "my_lambda_function"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  filename         = "lambda_payload.zip"  
+  function_name    = "my_lambda_function-1"
   role             = data.aws_iam_role.lambda.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
