ef18a3ed241862a2feef45af85082750fa1b90e2
Himanshu Jain <54056387+himanshujainhj@users.noreply.github.com>
2024-10-24 16:17:28 +0530

    added variable for subnet_cidr

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create a Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = data.aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24" 
+  cidr_block        = var.subnet_cidr 
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "subnet_cidr" {
+  default = "10.0.45.0/24"
+}
